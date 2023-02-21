f2a0b3d06da8494106e68b5c85dabe3a2f82c689
Patrik Sandahl <patrik.sandahl@gmail.com>
2023-02-21 11:53:18 +0100

    Simple prediction.

--- symdetect/__main__.py
+++ symdetect/__main__.py
@@ -8,13 +8,14 @@ import symdetect.unet as unet
 
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow.keras as keras
 from tensorflow.keras.preprocessing.image import img_to_array
 
+image_size = (256, 256)
+batch_size = 5
 
-def train_gryphon(dir: pathlib.Path) -> None:
-    image_size = (256, 256)
-    batch_size = 5
 
+def train_gryphon(dir: pathlib.Path, model_path: pathlib.Path) -> None:
     # Load files from the dataset.
     files = id.gryphon_image_paths(dir)
 
@@ -40,35 +41,65 @@ def train_gryphon(dir: pathlib.Path) -> None:
     print(
         f' Validation files={len(validation_set)}, validation batches={len(validation_seq)}')
 
-    unet.build_and_train(train_seq=train_seq, validation_seq=validation_seq,
-                         image_size=image_size, model_path=pathlib.Path('models/firsttry.h5'))
+    if model_path is not None:
+        # Train model.
+        unet.build_and_train(train_seq=train_seq, validation_seq=validation_seq,
+                             image_size=image_size, model_path=model_path)
+    else:
+        # Visualize a few images from a batch.
+        X, Y = train_seq[random.randint(0, len(train_seq))]
 
-    # Visualize a few images from a batch.
-    # X, Y = train_seq[random.randint(0, len(train_seq))]
+        plt.figure(figsize=(9, 9))
+        for num, idx in enumerate([0, 1, 2, 3]):
+            plt.subplot(4, 2, 2 * num + 1)
+            plt.imshow(X[idx], cmap='gray')
+            plt.title('image with HUD')
+            plt.axis('off')
 
-    # plt.figure(figsize=(9, 9))
-    # for num, idx in enumerate([0, 3, 6, 14]):
-    #    plt.subplot(4, 2, 2 * num + 1)
-    #    plt.imshow(X[idx], cmap='gray')
-    #    plt.title('image with HUD')
-    #    plt.axis('off')
+            plt.subplot(4, 2, 2 * num + 2)
+            plt.imshow(Y[idx], cmap='gray')
+            plt.title('mask')
+            plt.axis('off')
 
-    #    plt.subplot(4, 2, 2 * num + 2)
-    #    plt.imshow(Y[idx], cmap='gray')
-    #    plt.title('mask')
-    #    plt.axis('off')
+        plt.show()
 
-    # plt.show()
+
+def predict_mask(image_path: pathlib.Path, model_path: pathlib.Path) -> None:
+    im = keras.utils.load_img(image_path, color_mode='grayscale',
+                              target_size=image_size, interpolation='bicubic')
+    im = img_to_array(im) / 255.
+
+    model = keras.models.load_model(model_path)
+
+    plt.figure(figsize=(9, 9))
+    plt.subplot(1, 2, 1)
+    plt.imshow(im, cmap='gray')
+    plt.axis('off')
+
+    im = np.expand_dims(im, 0)
+    result = model.predict(im)
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(result[0], cmap='gray')
+    plt.axis('off')
+
+    plt.show()
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--gryphon', type=pathlib.Path,
                         help='Train using Gryphon dataset')
+    parser.add_argument('--model', type=pathlib.Path,
+                        help='Path to a model file to read or write')
+    parser.add_argument('--predict', type=pathlib.Path,
+                        help='Path to image for which a mask will be predicted')
     opts = parser.parse_args()
 
     if opts.gryphon is not None:
-        train_gryphon(opts.gryphon)
+        train_gryphon(opts.gryphon, opts.model)
+    elif opts.predict is not None and opts.model is not None:
+        predict_mask(opts.predict, opts.model)
     else:
         parser.print_usage()
 
